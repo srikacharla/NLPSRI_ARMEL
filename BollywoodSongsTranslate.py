f2b809f4b5f5c3e20a86f0f7bf291790f3ce1f79
Harsha <srikacharla@gmail.com>
2021-04-26 19:33:47 -0400

    Reformated a file

--- BollywoodSongsTranslate.py
+++ BollywoodSongsTranslate.py
@@ -3,21 +3,21 @@ import pickle
 
 translator = Translator()
 
-BollywoodLyrics = pickle.load( open( "Datasets/BollywoodLyrics", "rb" ) )
+BollywoodLyrics = pickle.load(open("Datasets/BollywoodLyrics", "rb"))
 
 import nltk
+
 nltk.download('punkt')
 a = []
-count= 0
+count = 0
 for x in BollywoodLyrics:
-  y = x.split('\n')
-  a.append([])
-  for z in y:
-    if z is not None:
-      print(z)
-      a[count] =translator.translate(z,dest='en')
-  count+=1
+    y = x.split('\n')
+    a.append([])
+    for z in y:
+        if z is not None:
+            print(z)
+            a[count] = translator.translate(z, dest='en')
+    count += 1
 
-
-f = open("Datasets/translated_lyrics_combined",'wb')
-pickle.dump(a,f)
+f = open("Datasets/translated_lyrics_combined", 'wb')
+pickle.dump(a, f)
